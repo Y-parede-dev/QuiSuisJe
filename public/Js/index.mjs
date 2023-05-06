6d3bcd88b9045d78a2483f7d0019a6be11c263c5
Y-parede-dev <parede.yoan.amcs@gmail.com>
2023-05-07 01:03:16 +0200

    DELETE import firebase

--- public/Js/index.mjs
+++ public/Js/index.mjs
@@ -2,7 +2,7 @@ const Game = document.getElementById("Game");
 const ResetBtn = document.getElementById("resetBtn");
 const CreateGameBtn = document.getElementById("createGame")
 const Loader = document.getElementById("loader")
-let storageRef = firebase.storage.ref("images");
+// let storageRef = firebase.storage.ref("images");
 // import * as images from "/public/Assets/images"
 // console.log(images)
 // import images from '../Assets/images'
@@ -16,7 +16,7 @@ let storageRef = firebase.storage.ref("images");
 // dirImages.forEach(image=>{
 //     console.log(image)
 // })
-console.log(storageRef)
+// console.log(storageRef)
 // fetch("gs://quisuisje-95869.appspot.com")
 //     .then(res=>res.json())
 //     .then(res=> console.log(res))
