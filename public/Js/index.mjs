85ea2c501d6f10961aaa4538845f712b7e641927
Y-parede-dev <parede.yoan.amcs@gmail.com>
2023-05-07 04:45:55 +0200

    mise en place d'un web socket qui remplace le serv

--- public/Js/index.mjs
+++ public/Js/index.mjs
@@ -1,37 +1,20 @@
+const HOST = location.origin.replace(/^http/, 'ws')
+const myHeaders = new Headers();
+const ws = new WebSocket(`${HOST}/ws`);
 const Game = document.getElementById("Game");
 const ResetBtn = document.getElementById("resetBtn");
 const CreateGameBtn = document.getElementById("createGame")
 const Loader = document.getElementById("loader")
-// let storageRef = firebase.storage.ref("images");
-// import * as images from "/public/Assets/images"
-// console.log(images)
-// import images from '../Assets/images'
 
-// console.log(images)
-// for (let image in images){
-//     console.log(`<img src=${image}/>`)
-// }
 
-// const dirImages = fs.readdirSync(__dirname)
-// dirImages.forEach(image=>{
-//     console.log(image)
-// })
-// console.log(storageRef)
-// fetch("gs://quisuisje-95869.appspot.com")
-//     .then(res=>res.json())
-//     .then(res=> console.log(res))
 let DataApi = [];
-const myHeaders = new Headers();
 
 const myInit = { method: 'GET',
                  headers: myHeaders,
                  mode: 'cors',
                  Cache: "no-cache" };
 
-// data = data[0]
-// data.forEach(e=>{ console.log(e)})
-console.log(DataApi.length)
-DataApi.map(e=>console.log(e))
+
 const loading = document.createElement('P');
 const loadElt = ["l","o","a","d","i","n","g"]
 
@@ -73,25 +56,30 @@ ResetBtn.addEventListener('click', () => {
     sessionStorage.setItem("reload", true)
 })
 const url = "/public/Assets/images"
-// Creation du jeux au clique sur le Bouton
+
+
 CreateGameBtn.addEventListener('click', () => {
-    CreateGameBtn.remove()
-    fetch(url, myInit)
-    .then((res) => res.json())
-    .then((res) => {
-        res.map(e=>{
-            // console.log(e.name)
-            DataApi.push(e.name)
-        })
-        createGameBoard(Game, url, DataApi)
-        console.log(DataApi)
+    
+    ws.send('getGameData');
+
+    ws.addEventListener('message', function (event) {
+        console.log(event)
+        const data = JSON.parse(event.data);
+        const srcImg = data.map((e) => e.name);
+
+        CreateGameBtn.remove();
+        createGameBoard(Game, url, srcImg);
+        console.log(srcImg);
     })
-    .catch((err) => console.error(err))
+    
+    ws.onclose = () => {
+        console.log('WebSocket closed');
+        ws.close()
+    }
 })
 
 const createGameBoard = (parent, urlBase, srcImg) => {
     const UL = document.createElement("UL");
-    // console.log(srcImg)
     for(let i=0;i<24;i++){
         const LI = document.createElement("LI");
         const DIV = document.createElement("DIV");
--- server.js
+++ server.js
@@ -1,89 +1,64 @@
-const express = require("express")
-const fs = require('fs')
-const http = require('http')
-const cors = require("cors")
-const morgan = require('morgan')
-const bodyParser = require("body-parser")
-const router = express.Router()
-const App = express()
-const port = 3000 | process.env.PORT
+const express = require("express");
+const fs = require('fs');
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const router = express.Router();
+const server = express();
+const expressWs = require('express-ws')
+const WebSocket = require('ws');
+const publicDir = fs.readdirSync(__dirname + "/public/Assets/images");
+let tt = [];
 
-const publicDir = fs.readdirSync(__dirname + "/public/Assets/images")
-let tt = []
-const errorHandler = error => {  // gestion des erreurs
-    if (error.syscall !== 'listen') {
-      throw error;
-    };
-    const address = Server.address();
-    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
-    switch (error.code) {
-      case 'EACCES':
-        console.error(bind + ' requires elevated privileges.');
-        process.exit(1);
-        break;
-      case 'EADDRINUSE':
-        console.error(bind + ' is already in use.');
-        process.exit(1);
-        break;
-      default:
-        throw error;
-    };
-  };
+expressWs(server);
+
 publicDir.forEach(file=>{
-    tt.push({name:file})
-    let t = __filename
-    // fs.readFileSync(file.__filename)
-    // console.log(file)
-})
-router.get('/', (req,res)=>{
-    // res.type('html')
-    
-    res.sendFile(__dirname + '/public/index.html')
-})
-router.get('/public/Assets/images', (req,res)=>{
-    // res.type('application/json')
-    // res.send(()=>{
-    //     publicDir.forEach(file=>{
-    //         return file
-    //         console.log(file)
-    //     })
-    // })
-    res.status(200)
-    res.send(tt)
-})
-// router
-//     .get("/",(req, res) => {
-//         res.render('home', fs.readdirSync('./index.html',))
-//     })
+    tt.push({name:file});
+});
 
-App.use(morgan('combined')); 
-App.use(cors()); 
-App.use(bodyParser.json()); 
-App.use(bodyParser.urlencoded({
-    extended: true
-}));
-App.use((req, res, next) => {
+router.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+
+server.use(cors()); 
+server.use(bodyParser.json()); 
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
-App.use(router)
-App.use(express.static(__dirname + '/public'));
-App.use(express.static(__dirname + '/public/Assets/images'));
+server.use(router);
+server.use(express.static(__dirname + '/public'));
+server.use(express.static(__dirname + '/public/Assets/images'));
 
-// App.listen(port,()=>{
-//     console.log('Server app listening on port ' + port)
-// })
-const Server = http.createServer(App)
-Server.on('error', errorHandler);  // si le serveur a une erreur sa nous la renvoie
+const wss = new WebSocket.Server({ server });
+const wsRouter = express.Router();
 
-Server.on('listening', () => { //si tout est ok, ecoute l'adresse et on y ajoute le port
-  const address = Server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-  
-  console.log('Listening on ' + bind); // on renvoie ecoute sur et le port a la console pour dire que tout c'est bien passer
+wsRouter.ws('/ws', (ws, req) => {
+  console.log('Client connected');
+
+  ws.on('message', (message) => {
+    console.log('Received message:', message);
+    if (message === 'getGameData') {
+        ws.send(JSON.stringify(tt));
+      wss.clients.forEach(client => {
+        console.log(client)
+          client.send(JSON.stringify(tt));
+      });
+    }
+  });
+
+  ws.on('close', () => console.log('Client disconnected'));
 });
 
-// on applique la fonction listen au Server avec le port en argument
-Server.listen(port);
+wsRouter.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+server.use(wsRouter);
+
+const port = process.env.PORT || 437;
+
+server.listen(port, ()=> {console.log('Serveur ouvert sur le port ' + port)});
